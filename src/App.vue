35ec820b0933751dbcb82f5f27d464a9da3173d0
bbMicobo <bbMicobo>
2019-12-10 11:59:35 +0100

    first commit1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app">
+    <div>
+    <button
+    v-for="entry in languages" 
+    :key="entry.title" 
+    @click="changeLocale(entry.language)">        
+    <flag :iso="entry.flag" v-bind:squared=false /> 
+    {{entry.title}}
+    </button>
+    </div>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,11 +16,26 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import i18n from '@/plugins/i18n';
 
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      languages: [
+        { flag: 'us', language: 'en', title: 'English' },
+        { flag: 'es', language: 'es', title: 'Español' }       
+      ]    
+    };
+  },
+  // This method will change the locale to the language on the button the user clicks
+  methods: {
+    changeLocale(localeLang) {
+      i18n.locale = localeLang; 
+    }
   }
 }
 </script>
@@ -25,4 +49,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+button {
+  padding: 15px;    
+  border: 1px solid green;    
+  font-size: 18px;    
+  margin: 15px;
+  }
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <!-- <h1>{{ msg }}</h1> -->
+    <h1>{{ $t('welcomeMsg') }}</h1>
+    <p>{{ $t('guide') }}<br> </p>
+     <p>  {{ $t('checkout') }} </p>   <h3>{{ $t('ecosystem') }}</h3>         
+
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
